19e224a0c502cc7eb2c0ea3b857fe828cc748490
fjodoin <fjodoin@protonmail.com>
2020-11-06 19:41:12 -0500

    added line chart

--- wes_doyle_tutorial/spend-smart/src/app/charts/line-chart/line-chart.component.ts
+++ wes_doyle_tutorial/spend-smart/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [1300, 1100, 1000, 1400, 1500, 1200], label: 'Income' },
+  { data: [600, 700, 550, 800, 1000, 1800], label: 'Expenses' },
+  { data: [100, 300, 400, 500, 700, 800], label: 'Savings' }
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +17,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartLegend: true;
+  lineChartColors = LINE_CHART_COLORS;
+  lineChartType = 'line';
 
   ngOnInit(): void {
   }
